a3d7a8d44a14427f8044c098b0db286ff9dc2ed2
Stephen M. Moraco <stephen@ironsheep.biz>
2020-11-05 02:27:34 -0700

    first working OUTLINE generator

--- spin2/src/spin2.extension.ts
+++ spin2/src/spin2.extension.ts
@@ -8,17 +8,13 @@ export function activate(context: vscode.ExtensionContext) {
 
     context.subscriptions.push(
         vscode.languages.registerDocumentSymbolProvider(
-            {scheme: "file", language: "spin2-config"},
+            {scheme: "file", language: "spin2"},
             new Spin2ConfigDocumentSymbolProvider()
         )
     );
 }
 
 class Spin2ConfigDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
-
-    private format(cmd: string):string{
-        return cmd.substr(1).toLowerCase().replace(/^\w/, c => c.toUpperCase())
-    }
 
     public provideDocumentSymbols(
         document: vscode.TextDocument,
@@ -27,90 +23,54 @@ class Spin2ConfigDocumentSymbolProvider implements vscode.DocumentSymbolProvider
         return new Promise((resolve, _reject) =>
         {
             let symbols: vscode.DocumentSymbol[] = [];
-            let nodes = [symbols]
-            let inside_marker = false
-            let inside_run = false
-            let inside_userinput = false
-
-            let symbolkind_marker = vscode.SymbolKind.Field
-            let symbolkind_run = vscode.SymbolKind.Event
-            let symbolkind_cmd = vscode.SymbolKind.Function
 
             for (var i = 0; i < document.lineCount; i++) {
                 var line = document.lineAt(i);
-
-                let tokens = line.text.split(" ")
-
-                if (line.text.startsWith("CON") || line.text.startsWith("DAT") || line.text.startsWith("VAR") || line.text.startsWith("OBJ")) {
-
-                    let marker_symbol = new vscode.DocumentSymbol(
-                        this.format(tokens[0]) + " " + tokens[1],
-                        'Component',
-                        symbolkind_marker,
-                        line.range, line.range)
-
-
-                    nodes[nodes.length-1].push(marker_symbol)
-                    //if (!inside_marker) {
-                    //    nodes.push(marker_symbol.children)
-                    //    inside_marker = true
-                    //}
-                    // marker_symbol.children.push(_boot)
+                let linePrefix : string = line.text
+                if (line.text.length > 3) {
+                    linePrefix = linePrefix.substring(0,3).toUpperCase()
                 }
-                else if (line.text.startsWith("#END_COMP")) {
-                    // TODO check if nodes has length 1 before popping.
-                    if (inside_marker) {
-                        nodes.pop()
-                        inside_marker = false
+
+                if (linePrefix.startsWith("CON") || linePrefix.startsWith("DAT") || linePrefix.startsWith("VAR") || linePrefix.startsWith("OBJ")) {
+                    var section: string = line.text.trim()
+                    if (section.includes("'")) {
+                        const lineParts: string[] = section.split("'")
+                        section = lineParts[0].trim()
                     }
-                }
-                //else if (line.text.startsWith("#RUN") || line.text.startsWith("#END")) {
-//
-                //    let run_symbol = new vscode.DocumentSymbol(
-                //        this.format(tokens[0]),
-                //        'Session separator',
-                //        symbolkind_run,
-                //        line.range, line.range)
-//
-                //    if (inside_run) {
-                //        nodes.pop()
-                //    }
-//
-                //    nodes[nodes.length-1].push(run_symbol)
-                //    nodes.push(run_symbol.children)
-                //    inside_run = true
-                //}
-                //else if (line.text.startsWith("#USERINPUTBEGIN")) {
-//
-                //    let user_symbol = new vscode.DocumentSymbol(
-                //        this.format(tokens[0]),
-                //        'User module',
-                //        vscode.SymbolKind.Interface,
-                //        line.range, line.range)
-
-
-                //    nodes[nodes.length-1].push(user_symbol)
-                //    if (!inside_userinput) {
-                //        nodes.push(user_symbol.children)
-                //        inside_userinput = true
-                //    }
-                    // marker_symbol.children.push(_boot)
-                //}
-                //else if (line.text.startsWith("#USERINPUTEND")) {
-                //    // TODO check if nodes has length 1 before popping.
-                //    if (inside_userinput) {
-                //        nodes.pop()
-                //        inside_userinput = false
-                //    }
-                //}
-                else if (line.text.startsWith("PUB") || line.text.startsWith("PRI")) {
-                    let cmd_symbol = new vscode.DocumentSymbol(
-                        this.format(tokens[1]),
+                    let marker_symbol = new vscode.DocumentSymbol(
+                        linePrefix + section.substr(3),
                         '',
-                        symbolkind_cmd,
+                        vscode.SymbolKind.Field,
                         line.range, line.range)
 
-                    nodes[nodes.length-1].push(cmd_symbol)
+                    symbols.push(marker_symbol)
+                }
+                else if (linePrefix.startsWith("PUB") || linePrefix.startsWith("PRI")) {
+                    var methodScope: string = "Public"
+                    if (line.text.startsWith("PRI")) {
+                        methodScope = "Private"
+                    }
+                    var methodName: string = line.text.substr(3).trim()
+                    if (methodName.includes("'")) {
+                        const lineParts: string[] = methodName.split("'")
+                        methodName = lineParts[0].trim()
+                    }
+                    if (methodName.includes("{")) {
+                        const lineParts: string[] = methodName.split("{")
+                        methodName = lineParts[0].trim()
+                    }
+                    if (methodName.includes("|")) {
+                        const lineParts: string[] = methodName.split("|")
+                        methodName = lineParts[0].trim()
+                    }
+
+                    let cmd_symbol = new vscode.DocumentSymbol(
+                        linePrefix + ' ' + methodName,
+                        '',
+                        vscode.SymbolKind.Function,
+                        line.range, line.range)
+
+                    symbols.push(cmd_symbol)
                 }
             }
 
